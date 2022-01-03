4c408bb9490cc97298b6908705a4cf8c5c180427
elinlow <elin.andersson.lowenadler@gmail.com>
2022-01-03 19:53:02 +0100

    made changes to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,35 @@
 const closeModal = (isModalOpen, setModalOpen) => {
     setModalOpen(false);
-    if (isModalOpen) {
+    if (!isModalOpen) {
         document.getElementById("myModal").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
     }
 };
+
+const closeOutsideClick = event => {
+    const modal = document.getElementById("modal-wrapper");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 const Modal = ({ isModalOpen, setModalOpen }) => {
     return (
         <>
             {isModalOpen && (
-                <div className="modal-content">
-                    <span 
-                        className="close"
-                        onClick={() => {
-                            closeModal( isModalOpen, setModalOpen);
-                        }}
-                    >
-                        x
-                    </span>
-                    <p>No, this is wrong. Click on the sidebar button to learn more about Elin.</p>
+                <div id="modal-wrapper" onClick={(event) => closeOutsideClick(event)}>
+                    <div className="modal-content">
+                        <span 
+                            className="close"
+                            onClick={() => {
+                                closeModal(isModalOpen, setModalOpen);
+                            }}
+                        >
+                            x
+                        </span>
+                        <p>No, this is wrong. Click on the sidebar button to learn more about Elin.</p>
+                    </div>
                 </div>
             )}
         </>
--- src/components/ModalBtn.js
+++ src/components/ModalBtn.js
@@ -1,5 +1,6 @@
 const openModal = (isModalOpen, setModalOpen) => {
     setModalOpen(true);
+    document.getElementById("modal-wrapper").style.display = "block";
     if (!isModalOpen) {
         document.getElementById("myModal").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
